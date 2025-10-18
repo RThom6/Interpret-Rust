56172bad1705cde5ae76762d02c42794c7240c38
RThom6 <ryscai@icloud.com>
2025-10-18 17:37:53 +0100

    Added some file parsing/argument handling

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::{
+    env,
+    fs::{self},
+    io,
+    process::exit,
+};
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() > 1 {
+        eprintln!("Usage: rlox [script]");
+        exit(64);
+    } else if args.len() == 1 {
+        run_file(args.get(0).unwrap()); // no or because already checked if arg exists
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(path: &String) {
+    let file_contents: String =
+        fs::read_to_string(&path).unwrap_or_else(|_| panic!("Could not read file: {}", path));
+
+    run(file_contents);
+}
+
+fn run_prompt() {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+}
+
+fn run(contents: String) {}
