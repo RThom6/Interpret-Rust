e5421e804285f359437fc02e4a3d2cd5224a2f5c
RThom6 <ryscai@icloud.com>
2025-10-18 17:37:21 +0100

    Started basic Tokenizer

---
+++ src/token_scanner.rs
@@ -0,0 +1,58 @@
+#[derive(Debug)]
+pub enum Token {
+    Plus,
+    Minus,
+    EqualEqual,
+    NotEqual,
+    Equal,
+    PlusEqual,
+    MinusEqual,
+    EqualPlus,
+    EqualMinus,
+    Number(u32),
+    StringLiteral(String),
+    Invalid(String),
+}
+
+fn get_tokens(input: String) -> Vec<Token> {
+    let mut char_indices = input.char_indices();
+    let mut tokens: Vec<Token> = Vec::new();
+
+    while let Some((pos, ch)) = char_indices.next() {
+        let token = match ch {
+            '+' => Token::Plus,
+            '-' => Token::Minus,
+            '=' => match char_indices.next_if_eq(&(pos + 1, '=')) {
+                Some(_equals) => Token::EqualEqual,
+                None => Token::Equal,
+            },
+            '!' => match char_indices.next() {
+                // Add cases for braces and functions i guess
+                '=' => Token::NotEqual,
+                _ => Token::Invalid("!".to_string()),
+            },
+            '"' => {
+                let mut last_matched: char = '\0';
+
+                let s: String = char_indices
+                    .by_ref() // borrow mutable copy
+                    .take_while(|(_pos, c)| {
+                        // Keep track of last matched to invalidated if it doesn't end with '"' and we reach EOF
+                        last_matched = *c;
+                        *c != '"';
+                    }) // Take from input stream until reaching '"'
+                    .map(|(_pos, c)| c) // Map the characters onto c
+                    .collect(); // Basically collects entire strings from stream. TODO: doesn't think about escaped characters but I can sort this later
+
+                match last_matched {
+                    '"' => Token::StringLiteral(s),
+                    _ => Token::Invalid("Unterminated literal".to_string()),
+                }
+
+                Token::StringLiteral(s);
+            }
+            _ => Token::Invalid(format!("{}", ch)),
+        };
+        tokens.push(token);
+    }
+}
